a6ddeef56e7f4c027411b5ed975d78e3d3955d18
kirill <k_sapegin28112002@mail.ru>
2024-03-22 09:42:12 +0700

    lab3

--- src/components/LabsList.jsx
+++ src/components/LabsList.jsx
@@ -1,15 +1,13 @@
 import React from "react"
 import './comp.css'
-
 const LabList = () =>{
     return(
         <div className="labslist_main">
             <h2>Это список лабораторных работ</h2>
-            <p></p>
             <ul>
-                <li>2 лабораторная работа</li>
-                <li>3 лабораторная работа </li>
-                <li>4 лабораторная работа</li>
+                <li><a href="#">2 лабораторная</a></li>
+                <li><a href="#">3 лабораторная</a></li>
+                <li><a href="#">4 лабораторная</a></li>
             </ul>
         </div>
     )
